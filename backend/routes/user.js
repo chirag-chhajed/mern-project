6f1e7369698a7eeb5458c8762ea07ae2e740d25d
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-01-11 12:47:26 +0530

    backend added

---
+++ backend/routes/user.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+const {loginUser,signupUser} = require('../controllers/userController')
+
+const router = Router()
+
+// login route
+router.post('/login',loginUser)
+
+// signup route
+router.post('/signup',signupUser)
+
+module.exports = router
